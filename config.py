46a9c8fe634232d0403cddf01c2c00c0486e5de2
ᴛᴏɴɪ - ᴇx <103916927+Toni880@users.noreply.github.com>
2022-06-17 19:07:02 +0700

    Ah ah ah💦

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-from base64 import b64decode
 
 load_dotenv()
 
@@ -13,8 +12,5 @@ LOGO_PRIME = os.getenv("LOGO_PRIME", "https://telegra.ph/file/7e0c2450664bfc3042
 LOG_CHAT = int(os.getenv("LOG_CHAT"))
 HEROKU_API = os.getenv("HEROKU_API", None)
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", None)
-GIT_TOKEN = os.getenv(
-  "GIT_TOKEN", b64decode(
-    "Z2hwX3RmbGNDUlhNSEw3MmxoQ0w5Z2c1b3Bkb3VLbVl6cTFRejA1dw==").decode(
-    "utf-8"),)
+GIT_TOKEN = os.getenv("GIT_TOKEN", "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")
 PM_LOGO = os.getenv("PM_LOGO")
