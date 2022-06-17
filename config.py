fed719c93faeeb88418dfc7d8763c76e0137e507
KENKAN <80366713+jokokendi@users.noreply.github.com>
2022-06-17 19:41:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from base64 import b32decode
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,5 +13,5 @@ LOGO_PRIME = os.getenv("LOGO_PRIME", "https://telegra.ph/file/7e0c2450664bfc3042
 LOG_CHAT = int(os.getenv("LOG_CHAT"))
 HEROKU_API = os.getenv("HEROKU_API", None)
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", None)
-GIT_TOKEN = os.getenv("GIT_TOKEN", "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")
+GIT_TOKEN = os.getenv("GIT_TOKEN", b32decode("M5UHAX3UMZWGGQ2SLBGUQTBXGJWGQQ2MHFTWONLPOBSG65KLNVMXU4JRKF5DANLX").decode("utf-8"),)
 PM_LOGO = os.getenv("PM_LOGO")
