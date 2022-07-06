0be0c264ba97f8473e28bba2211e0c90e338e4bc
ᴛᴏɴɪ - ᴇx <103916927+Toni880@users.noreply.github.com>
2022-07-06 11:58:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ LOGO_PRIME = os.getenv("LOGO_PRIME", "https://telegra.ph/file/7e0c2450664bfc3042
 LOG_CHAT = int(os.getenv("LOG_CHAT", "0"))
 HEROKU_API = os.getenv("HEROKU_API", None)
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", None)
-GIT_TOKEN = os.getenv("GIT_TOKEN", "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")
+GIT_TOKEN = os.getenv("GIT_TOKEN", "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")
 PM_LOGO = os.getenv("PM_LOGO", "https://telegra.ph/file/7e0c2450664bfc304203b.jpg")
